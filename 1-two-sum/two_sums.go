414e88cf70c46290402af21e17fcb55280fc0f80
Thomas D <mvcatsifma@gmail.com>
2021-05-21 16:48:51 +0200

    Two sum second iteration

--- 1-two-sum/two_sums.go
+++ 1-two-sum/two_sums.go
@@ -1,28 +1,27 @@
 package main
 
 func twoSum(nums []int, target int) []int {
-	// create hashmap num -> idx
-	// iterate over  nums
-	// drop nums > target
-	// save num + idx
-	// for each key, subtract from target, lookup result
-	// if found return key idx, result idx
-	// if not found, continue
-
-	numToIndex := make(map[int]int)
+	numToIndex := make(map[int][]int)
 
 	for idx, num := range nums {
-		if num > target {
-			continue
+		if idxs, ok := numToIndex[num]; !ok {
+			numToIndex[num] = []int{idx}
+		} else {
+			numToIndex[num] = append(idxs, idx)
 		}
-
-		numToIndex[num] = idx
 	}
 
-	for num1, idx1 := range numToIndex {
-		diff := target - num1
-		if idx2, ok := numToIndex[diff]; ok {
-			return []int{idx1, idx2}
+	for num, i0 := range numToIndex {
+		if num*2 == target && len(i0) > 1 {
+			return []int{i0[0], i0[1]}
+		}
+
+		sub := target - num
+
+		if i1, ok := numToIndex[sub]; ok {
+			if i0[0] != i1[0] {
+				return []int{i0[0], i1[0]}
+			}
 		}
 	}
 
--- 1-two-sum/two_sums_test.go
+++ 1-two-sum/two_sums_test.go
@@ -15,6 +15,7 @@ func TestTwoSum(t *testing.T) {
 		{"Example 1", []int{2, 7, 11, 15}, 9, []int{0, 1}},
 		{"Example 2", []int{3, 2, 4}, 6, []int{1, 2}},
 		{"Example 3", []int{3, 3}, 6, []int{0, 1}},
+		{"Testcase 1", []int{-3, 4, 3, 90}, 0, []int{0, 2}},
 	}
 
 	for _, tt := range sumTests {
