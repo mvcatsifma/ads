cfc6fa38879b33152c56e8c9294029d72ab2cb3d
Thomas D <mvcatsifma@gmail.com>
2021-05-21 22:13:20 +0200

    Refactor for runtime and memory

--- 1-two-sum/two_sums.go
+++ 1-two-sum/two_sums.go
@@ -1,29 +1,13 @@
 package main
 
 func twoSum(nums []int, target int) []int {
-	numToIndex := make(map[int][]int)
-
-	for idx, num := range nums {
-		if idxs, ok := numToIndex[num]; !ok {
-			numToIndex[num] = []int{idx}
-		} else {
-			numToIndex[num] = append(idxs, idx)
-		}
-	}
-
-	for num, i0 := range numToIndex {
-		if num*2 == target && len(i0) > 1 {
-			return []int{i0[0], i0[1]}
-		}
-
+	var numToIdx = make(map[int]int)
+	for idx2, num := range nums {
 		sub := target - num
-
-		if i1, ok := numToIndex[sub]; ok {
-			if i0[0] != i1[0] {
-				return []int{i0[0], i1[0]}
-			}
+		if idx1, ok := numToIdx[sub]; ok {
+			return []int{idx1, idx2}
 		}
+		numToIdx[num] = idx2
 	}
-
 	return []int{}
 }
