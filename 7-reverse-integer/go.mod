053b24888d9dc8b2f26e0ae747958f7975d0c262
Thomas D <mvcatsifma@gmail.com>
2021-07-05 16:43:34 +0200

    Add solution to #1 reverse integer

---
+++ 7-reverse-integer/go.mod
@@ -0,0 +1,3 @@
+module reverseint
+
+go 1.16
---
+++ 7-reverse-integer/reverse_test.go
@@ -0,0 +1,46 @@
+package main
+
+import "testing"
+
+func Test(t *testing.T) {
+	tests := []struct {
+		name     string
+		in       int
+		expected int
+	}{
+		{
+			name:     "Example 1",
+			in:       123,
+			expected: 321,
+		},
+		{
+			name:     "Example 2",
+			in:       -123,
+			expected: -321,
+		},
+		{
+			name:     "Example 3",
+			in:       120,
+			expected: 21,
+		},
+		{
+			name:     "Example 4",
+			in:       0,
+			expected: 0,
+		},
+		{
+			name:     "TestCase 12",
+			in:       1534236469,
+			expected: 0,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actual := reverse(test.in)
+
+			if actual != test.expected {
+				t.Errorf("expected %d, got %d", test.expected, actual)
+			}
+		})
+	}
+}
