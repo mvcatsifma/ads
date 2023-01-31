197c7c2f20bdecdde66a44b29e448190bad60d85
Thomas Delnoij <thomas.delnoij@crowdstrike.com>
2023-01-31 09:59:00 +0100

    complete #66

---
+++ 66-plus-one/plus.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	//digits := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	digits := []int{9}
+	fmt.Println(digits)
+	digits = plusOne(digits)
+	fmt.Println(digits)
+}
+
+func plusOne(digits []int) []int {
+	l := len(digits)
+	ones := make([]int, l)
+	ones[l-1] = 1
+
+	for i := l - 1; i >= 0; i-- {
+		d := digits[i]
+		o := ones[i]
+		s := d + o
+		if s == 10 {
+			digits[i] = 0
+			if i-1 >= 0 {
+				ones[i-1] = 1
+			} else {
+				digits = append([]int{1}, digits...)
+			}
+		} else {
+			digits[i] = s
+		}
+	}
+
+	return digits
+}
