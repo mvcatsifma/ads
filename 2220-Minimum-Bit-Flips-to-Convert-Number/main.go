7c3955a023c695775fa662183632ee564ea9ca3a
Thomas Delnoij <thomas.delnoij@crowdstrike.com>
2023-02-07 08:25:24 +0100

    complete #2220

--- 2220-Minimum-Bit-Flips-to-Convert-Number/main.go
+++ 2220-Minimum-Bit-Flips-to-Convert-Number/main.go
@@ -1,9 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
+func main() {
+	fmt.Println(minBitFlips(10, 7))
+	fmt.Println(minBitFlips(3, 4))
 }
 
 func minBitFlips(start int, goal int) int {
-	return -1
+	s := strconv.FormatInt(int64(start^goal), 2)
+
+	return strings.Count(s, "1")
 }
