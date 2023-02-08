e7be6948d939067307f2ff901e6b924f93c7eac8
Thomas Delnoij <thomas.delnoij@crowdstrike.com>
2023-02-08 10:14:43 +0100

    update #338

--- 338-Counting-Bits/counting.go
+++ 338-Counting-Bits/counting.go
@@ -1,16 +1,13 @@
 package main
 
 import (
-	"strconv"
-	"strings"
+	"math/bits"
 )
 
 func countBits(n int) []int {
 	var ans []int
 	for i := 0; i <= n; i++ {
-		fi := strconv.FormatInt(int64(i), 2)
-		c := strings.Count(fi, "1")
-		ans = append(ans, c)
+		ans = append(ans, bits.OnesCount(uint(i)))
 	}
 	return ans
 }
