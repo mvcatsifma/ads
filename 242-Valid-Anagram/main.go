beb01ac99716fcfe11c8f43422fcb5b57d433acb
mvcatsifma <mvcatsifma@users.noreply.github.com>
2025-06-20 11:03:46 +0200

    Solves 242. Valid Anagram

---
+++ 242-Valid-Anagram/main.go
@@ -0,0 +1,20 @@
+package main
+
+func isAnagram(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+	m := make(map[string]int)
+	for _, v := range s {
+		m[string(v)]++
+	}
+
+	for _, v := range t {
+		m[string(v)]--
+		if m[string(v)] < 0 {
+			return false
+		}
+	}
+
+	return true
+}
