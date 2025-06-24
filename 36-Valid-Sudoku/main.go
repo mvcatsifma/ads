2c43bdd52986b585ec7cb71544ca1d358c935e75
mvcatsifma <mvcatsifma@users.noreply.github.com>
2025-06-24 16:28:01 +0200

    Solve 36. - optimized approach using hashmaps

--- 36-Valid-Sudoku/main.go
+++ 36-Valid-Sudoku/main.go
@@ -6,47 +6,41 @@ type key struct {
 }
 
 func isValidSudoku(board [][]byte) bool {
-	var columns = make([][]byte, 9)
-	boxes := make(map[key][]byte)
+	const N = 9
+	rows := make([]map[byte]bool, N)
+	columns := make([]map[byte]bool, N)
+	boxes := make(map[key]map[byte]bool, N)
+	for i := 0; i < N; i++ {
+		rows[i] = make(map[byte]bool)
+		columns[i] = make(map[byte]bool)
+	}
 
-	// check if each row is valid
-	for ri := 0; ri < 9; ri++ {
-		row := board[ri]
-		if !isValid(row) {
-			return false
-		}
-		for ci := 0; ci < 9; ci++ {
-			v := row[ci]
-			columns[ci] = append(columns[ci], v)
-			k := key{x: ci / 3, y: ri / 3} // keys are 0...8, unique for each box
-			boxes[k] = append(boxes[k], v)
+	for ri := 0; ri < N; ri++ {
+		for ci := 0; ci < N; ci++ {
+			val := board[ri][ci]
+			if val == '.' {
+				continue
+			}
+			if rows[ri][val] {
+				return false
+			}
+			rows[ri][val] = true
+
+			if columns[ci][val] {
+				return false
+			}
+			columns[ci][val] = true
+
+			k := key{ri / 3, ci / 3}
+			if boxes[k] == nil {
+				boxes[k] = make(map[byte]bool)
+			}
+			if boxes[k][val] {
+				return false
+			}
+			boxes[k][val] = true
 		}
 	}
 
-	// check if each column is valid
-	for _, column := range columns {
-		if !isValid(column) {
-			return false
-		}
-	}
-
-	// check if each box is valid
-	for _, box := range boxes {
-		if !isValid(box) {
-			return false
-		}
-	}
-
-	return true
-}
-
-func isValid(cells []byte) bool {
-	m := make(map[byte]bool)
-	for _, value := range cells {
-		if _, ok := m[value]; ok && value != '.' { // . is allowed multiple times
-			return false
-		}
-		m[value] = true
-	}
 	return true
 }
