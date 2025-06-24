30c8a474a74402c9e782ca2c54ee9f2c01aed57a
mvcatsifma <mvcatsifma@users.noreply.github.com>
2025-06-24 13:36:29 +0200

    Solve 238 with the O(1) space approach

--- 238-Product-of-Array-Except-Self/main.go
+++ 238-Product-of-Array-Except-Self/main.go
@@ -1,22 +1,18 @@
 package main
 
 func productExceptSelf(nums []int) []int {
-	var leftProduct, rightProduct []int
+	var leftProduct []int
 	leftProduct = make([]int, len(nums))
 	leftProduct[0] = 1
 	for i := 1; i < len(nums); i++ {
 		leftProduct[i] = leftProduct[i-1] * nums[i-1]
 	}
 
-	rightProduct = make([]int, len(nums))
-	rightProduct[len(nums)-1] = 1
-	for i := len(nums) - 2; i >= 0; i-- {
-		rightProduct[i] = rightProduct[i+1] * nums[i+1]
-	}
-
 	result := make([]int, len(nums))
-	for i := 0; i < len(nums); i++ {
-		result[i] = leftProduct[i] * rightProduct[i]
+	rightProduct := 1
+	for i := len(nums) - 1; i >= 0; i-- {
+		result[i] = leftProduct[i] * rightProduct
+		rightProduct = rightProduct * nums[i]
 	}
 
 	return result
