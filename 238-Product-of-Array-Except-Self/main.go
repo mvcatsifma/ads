a9bfe512c59e1521d58f9e51ac8d7b5b83e04f59
mvcatsifma <mvcatsifma@users.noreply.github.com>
2025-06-24 13:26:36 +0200

    Solve 238 using left and right product lists
    Time complexity: O(N) - linear.

--- 238-Product-of-Array-Except-Self/main.go
+++ 238-Product-of-Array-Except-Self/main.go
@@ -1,16 +1,23 @@
 package main
 
 func productExceptSelf(nums []int) []int {
-	result := make([]int, len(nums))
-	for i := 0; i < len(result); i++ {
-		product := 1
-		for j := 0; j < len(nums); j++ {
-			if j == i {
-				continue
-			}
-			product = product * nums[j]
-		}
-		result[i] = product
+	var leftProduct, rightProduct []int
+	leftProduct = make([]int, len(nums))
+	leftProduct[0] = 1
+	for i := 1; i < len(nums); i++ {
+		leftProduct[i] = leftProduct[i-1] * nums[i-1]
 	}
+
+	rightProduct = make([]int, len(nums))
+	rightProduct[len(nums)-1] = 1
+	for i := len(nums) - 2; i >= 0; i-- {
+		rightProduct[i] = rightProduct[i+1] * nums[i+1]
+	}
+
+	result := make([]int, len(nums))
+	for i := 0; i < len(nums); i++ {
+		result[i] = leftProduct[i] * rightProduct[i]
+	}
+
 	return result
 }
