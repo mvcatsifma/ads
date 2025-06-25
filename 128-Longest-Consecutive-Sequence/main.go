eddbeba211e3b61889fb93e2154022e294938455
mvcatsifma <mvcatsifma@users.noreply.github.com>
2025-06-25 11:32:03 +0200

    Refactor 128 for readability and performance

--- 128-Longest-Consecutive-Sequence/main.go
+++ 128-Longest-Consecutive-Sequence/main.go
@@ -1,49 +1,45 @@
 package main
 
 import (
-	"fmt"
 	"sort"
 )
 
-func main() {
-	nums := []int{100, 4, 200, 1, 3, 2}
-	sort.Ints(nums)
-	fmt.Printf("%d\n", nums)
-}
-
 func longestConsecutive(nums []int) int {
-	if len(nums) == 0 {
+	if len(nums) == 0 { // return immediately
 		return 0
 	}
 
+	// Sort the input array
 	sort.Ints(nums)
-	seqs := make([][]int, 0)
-	seq := make([]int, 0)
-	prev := nums[:1][0]
-	seq = append(seq, prev)
-	for _, num := range nums[1:] {
-		if num-prev == 0 { // skip duplicate
+
+	seqs := make([][]int, 0) // list of sequences
+	seq := make([]int, 0)    // the current sequence
+	var prev int
+	for i, num := range nums {
+		if i == 0 { // first element in the input array is always added to the first sequence
+			seq = append(seq, num)
+			prev = num
 			continue
 		}
-		if num-prev == 1 { // add to sequence
+		diff := num - prev
+		switch diff {
+		case 0: // duplicate element
+			continue
+		case 1: // consecutive element
 			seq = append(seq, num)
-		} else { // start new sequence
+		default: // non-consecutive element, start a new sequence
 			seqs = append(seqs, seq)
 			seq = make([]int, 0)
 			seq = append(seq, num)
 		}
 		prev = num
 	}
-	seqs = append(seqs, seq)
+	seqs = append(seqs, seq) // add the last sequence to the sequences array
 
-	// TODO: time complexity of this loop is O(N),
-	// replace with sort which has an average time complexity of O(n log n).
-	var longest []int
-	for _, seq := range seqs {
-		if len(seq) > len(longest) {
-			longest = seq
-		}
-	}
+	// Sort the accumulated sequences.
+	sort.Slice(seqs, func(i, j int) bool {
+		return len(seqs[i]) > len(seqs[j])
+	})
 
-	return len(longest)
+	return len(seqs[0]) // return the length of the longest sequence
 }
