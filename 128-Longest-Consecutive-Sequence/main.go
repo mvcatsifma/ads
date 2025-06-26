5bf24af86aecd615e2254069d5fc4bb85175359d
mvcatsifma <mvcatsifma@users.noreply.github.com>
2025-06-26 10:08:18 +0200

    128. Refactor to HashMap

--- 128-Longest-Consecutive-Sequence/main.go
+++ 128-Longest-Consecutive-Sequence/main.go
@@ -1,45 +1,26 @@
 package main
 
-import (
-	"sort"
-)
-
 func longestConsecutive(nums []int) int {
-	if len(nums) == 0 { // return immediately
-		return 0
+	numSet := make(map[int]bool)
+	for _, num := range nums {
+		numSet[num] = true
 	}
-
-	var longestStreak int
-	var updateLongestStreak = func(currentStreak int) {
-		if currentStreak > longestStreak {
-			longestStreak = currentStreak
+	longestStreak := 0
+	for num := range numSet {
+		if _, exists := numSet[num-1]; !exists {
+			currentNum := num
+			currentStreak := 1
+			for {
+				if !numSet[currentNum+1] {
+					break
+				}
+				currentNum++
+				currentStreak++
+			}
+			if currentStreak > longestStreak {
+				longestStreak = currentStreak
+			}
 		}
 	}
-
-	// Sort the input array
-	sort.Ints(nums)
-
-	var currentStreak int
-	var prev int
-	for i, num := range nums {
-		if i == 0 { // first element in the input array starts the first streak
-			currentStreak++
-			prev = num
-			continue
-		}
-		diff := num - prev
-		switch diff {
-		case 0: // duplicate element
-			continue
-		case 1: // consecutive element
-			currentStreak++
-		default: // non-consecutive element, start a new streak
-			updateLongestStreak(currentStreak)
-			currentStreak = 1
-		}
-		prev = num
-	}
-	updateLongestStreak(currentStreak) // don't forget to update for the last streak
-
 	return longestStreak
 }
