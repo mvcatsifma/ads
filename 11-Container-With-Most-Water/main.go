752652c5abf1d2595e8f23eb406f6dda7bfee7dd
mvcatsifma <mvcatsifma@users.noreply.github.com>
2025-06-30 15:31:02 +0200

    Solve 11. using two pointers

--- 11-Container-With-Most-Water/main.go
+++ 11-Container-With-Most-Water/main.go
@@ -2,13 +2,22 @@ package main
 
 func maxArea(height []int) int {
 	var result int
-	for i := 0; i <= len(height)-1; i++ {
-		for j := i + 1; j <= len(height)-1; j++ {
-			area := min(height[i], height[j]) * (j - i)
-			if area > result {
-				result = area
-			}
+
+	lo := 0
+	hi := len(height) - 1
+
+	for lo < hi {
+		area := min(height[lo], height[hi]) * (hi - lo)
+		if area > result {
+			result = area
+		}
+
+		if height[lo] > height[hi] {
+			hi--
+		} else {
+			lo++
 		}
 	}
+
 	return result
 }
