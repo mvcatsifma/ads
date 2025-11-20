4edf12f7853f2ecaeb25358cdf7322cc9d2a85d8
mvcatsifma <mvcatsifma@users.noreply.github.com>
2025-11-20 10:27:52 +0100

    Fix bug in binary search midpoint calculation

--- 704-Binary-Search/main.go
+++ 704-Binary-Search/main.go
@@ -16,7 +16,7 @@ func search(nums []int, target int) int {
 	l, r := 0, len(nums)-1
 
 	for l <= r {
-		m := l + (r - l/2)
+		m := l + (r-l)/2
 		if nums[m] > target {
 			r = m - 1
 		} else if nums[m] < target {
