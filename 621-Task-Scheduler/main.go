ba1fff8c3c2f67519d80588cc1c39bf08d943092
mvcatsifma <mvcatsifma@users.noreply.github.com>
2025-12-29 12:39:28 +0100

    Fixes bug in 621

--- 621-Task-Scheduler/main.go
+++ 621-Task-Scheduler/main.go
@@ -61,7 +61,7 @@ func leastInterval(tasks []byte, n int) int {
 type IntHeap []int
 
 func (h IntHeap) Len() int           { return len(h) }
-func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h IntHeap) Less(i, j int) bool { return h[i] > h[j] }
 func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
 
 func (h *IntHeap) Push(x any) {
